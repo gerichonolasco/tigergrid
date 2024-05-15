ae61e8ea4d91a67c08a43e597b323a1211d53338
Gericho Nolasco <gerichojade.nolasco.cics@ust.edu.ph>
2024-05-15 08:46:12 +0800

    Edit Manage Questions fix

--- src/admin/pages/EditManageQuestions.tsx
+++ src/admin/pages/EditManageQuestions.tsx
@@ -5,78 +5,78 @@ import EditFormPage3 from "../components/Dashboard/EditForm/EditFormPage3";
 import EditFormPage4 from "../components/Dashboard/EditForm/EditFormPage4";
 
 interface NewQuestion {
-	newQuestion: string;
-	newInputType: string;
-	newDropdownChoices: string[];
+    newQuestion: string;
+    newInputType: string;
+    newDropdownChoices: string[];
 }
 
 const EditManageQuestions: FC = () => {
-	const [currentPage, setCurrentPage] = useState<number>(1);
-	const [questions, setQuestions] = useState<NewQuestion[]>([]);
-	const [question, setQuestion] = useState<NewQuestion>({
-		newQuestion: "",
-		newInputType: "",
-		newDropdownChoices: [],
-	});
-	const [error, setError] = useState<string>("");
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [questions, setQuestions] = useState<NewQuestion[]>([]);
+    const [question, setQuestion] = useState<NewQuestion>({
+        newQuestion: "",
+        newInputType: "",
+        newDropdownChoices: [],
+    });
+    const [error, setError] = useState<string>("");
 
-	const handleInputChange = (
-		e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
-		field: string
-	) => {
-		const { value } = e.target;
-		setQuestion((prevQuestion) => ({
-			...prevQuestion,
-			[field]: value,
-		}));
-	};
+    const handleInputChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+        field: keyof NewQuestion
+    ) => {
+        const { value } = e.target;
+        setQuestion((prevQuestion) => ({
+          ...prevQuestion,
+            [field]: value,
+        }));
+    };
 
-	const handleDropdownChange = (
-		index: number,
-		e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-	) => {
-		const { value } = e.target;
-		setQuestion((prevQuestion) => {
-			const updatedDropdownChoices = [...prevQuestion.newDropdownChoices];
-			updatedDropdownChoices[index] = value;
-			return {
-				...prevQuestion,
-				newDropdownChoices: updatedDropdownChoices,
-			};
-		});
-	};
+    const handleDropdownChange = (
+        index: number,
+        e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+        const { value } = e.target;
+        setQuestion((prevQuestion) => {
+            const updatedDropdownChoices = [...prevQuestion.newDropdownChoices];
+            updatedDropdownChoices[index] = value;
+            return {
+              ...prevQuestion,
+                newDropdownChoices: updatedDropdownChoices,
+            };
+        });
+    };
 
-	const addDropdownChoice = () => {
-		setQuestion((prevQuestion) => ({
-			...prevQuestion,
-			newDropdownChoices: [...prevQuestion.newDropdownChoices, ""],
-		}));
-	};
+    const addDropdownChoice = () => {
+        setQuestion((prevQuestion) => ({
+          ...prevQuestion,
+            newDropdownChoices: [...prevQuestion.newDropdownChoices, ""],
+        }));
+    };
 
-	const removeDropdownChoice = (index: number) => {
-		setQuestion((prevQuestion) => {
-			const updatedDropdownChoices = [...prevQuestion.newDropdownChoices];
-			updatedDropdownChoices.splice(index, 1);
-			return {
-				...prevQuestion,
-				newDropdownChoices: updatedDropdownChoices,
-			};
-		});
-	};
+    const removeDropdownChoice = (index: number) => {
+        setQuestion((prevQuestion) => {
+            const updatedDropdownChoices = [...prevQuestion.newDropdownChoices];
+            updatedDropdownChoices.splice(index, 1);
+            return {
+              ...prevQuestion,
+                newDropdownChoices: updatedDropdownChoices,
+            };
+        });
+    };
 
-	const addQuestion = () => {
-		if (question.newQuestion && question.newInputType) {
-			setQuestions((prevQuestions) => [...prevQuestions, question]);
-			setQuestion({
-				newQuestion: "",
-				newInputType: "",
-				newDropdownChoices: [],
-			});
-			setError("");
-		} else {
-			setError("Please complete all fields before adding.");
-		}
-	};
+    const addQuestion = () => {
+        if (question.newQuestion && question.newInputType) {
+            setQuestions((prevQuestions) => [...prevQuestions, question]);
+            setQuestion({
+                newQuestion: "",
+                newInputType: "",
+                newDropdownChoices: [],
+            });
+            setError(""); // Reset error message
+        } else {
+            setError("Please complete all fields before adding.");
+        }
+    };
 
 	const editQuestion = (index: number, updatedQuestion: NewQuestion) => {
 		setQuestions((prevQuestions) => {
@@ -85,106 +85,107 @@ const EditManageQuestions: FC = () => {
 			return newQuestions;
 		});
 		setQuestion({
-			newQuestion: updatedQuestion.newQuestion,
-			newInputType: updatedQuestion.newInputType,
-			newDropdownChoices: [...updatedQuestion.newDropdownChoices], // Make sure newDropdownChoices is copied
+			newQuestion: "", // Clear the question text
+			newInputType: "", // Clear the input type selection
+			newDropdownChoices: [], // Clear the dropdown choices array
 		});
 	};
+	
 
-	const deleteQuestion = (index: number) => {
-		setQuestions((prevQuestions) =>
-			prevQuestions.filter((_, i) => i !== index)
-		);
-	};
+    const deleteQuestion = (index: number) => {
+        setQuestions((prevQuestions) =>
+            prevQuestions.filter((_, i) => i!== index)
+        );
+    };
 
-	const handlePageChange = (e: ChangeEvent<HTMLSelectElement>) => {
-		setCurrentPage(Number(e.target.value));
-	};
+    const handlePageChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setCurrentPage(Number(e.target.value));
+    };
 
-	return (
-		<div className="flex justify-center items-center bg-cover bg-center bg-main-building">
-			<div>
-				<div className="text-gray-900 bg-gray-200 mt-8">
-					<div className="p-4 flex">
-						<h1 className="text-2xl">Edit Questions</h1>
-					</div>
-					<div className="px-3 py-4 flex justify-center">
-						<div className="mb-4">
-							<label htmlFor="pageSelector" className="mr-2">
-								Select Page:
-							</label>
-							<select
-								id="pageSelector"
-								value={currentPage}
-								onChange={handlePageChange}
-							>
-								<option value={1}>Page 1</option>
-								<option value={2}>Page 2</option>
-								<option value={3}>Page 3</option>
-								<option value={4}>Page 4</option>
-							</select>
-						</div>
-					</div>
-				</div>
-				{currentPage === 1 && (
-					<EditFormPage1
-						questions={questions}
-						question={question}
-						handleInputChange={handleInputChange}
-						handleDropdownChange={handleDropdownChange}
-						addDropdownChoice={addDropdownChoice}
-						removeDropdownChoice={removeDropdownChoice}
-						addQuestion={addQuestion}
-						editQuestion={editQuestion}
-						deleteQuestion={deleteQuestion}
-						error={error}
-					/>
-				)}
-				{currentPage === 2 && (
-					<EditFormPage2
-						questions={questions}
-						question={question}
-						handleInputChange={handleInputChange}
-						handleDropdownChange={handleDropdownChange}
-						addDropdownChoice={addDropdownChoice}
-						removeDropdownChoice={removeDropdownChoice}
-						addQuestion={addQuestion}
-						editQuestion={editQuestion}
-						deleteQuestion={deleteQuestion}
-						error={error}
-					/>
-				)}
-				{currentPage === 3 && (
-					<EditFormPage3
-						questions={questions}
-						question={question}
-						handleInputChange={handleInputChange}
-						handleDropdownChange={handleDropdownChange}
-						addDropdownChoice={addDropdownChoice}
-						removeDropdownChoice={removeDropdownChoice}
-						addQuestion={addQuestion}
-						editQuestion={editQuestion}
-						deleteQuestion={deleteQuestion}
-						error={error}
-					/>
-				)}
-				{currentPage === 4 && (
-					<EditFormPage4
-						questions={questions}
-						question={question}
-						handleInputChange={handleInputChange}
-						handleDropdownChange={handleDropdownChange}
-						addDropdownChoice={addDropdownChoice}
-						removeDropdownChoice={removeDropdownChoice}
-						addQuestion={addQuestion}
-						editQuestion={editQuestion}
-						deleteQuestion={deleteQuestion}
-						error={error}
-					/>
-				)}
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex justify-center items-center bg-cover bg-center bg-main-building">
+            <div>
+                <div className="text-gray-900 bg-gray-200 mt-8">
+                    <div className="p-4 flex">
+                        <h1 className="text-2xl">Edit Questions</h1>
+                    </div>
+                    <div className="px-3 py-4 flex justify-center">
+                        <div className="mb-4">
+                            <label htmlFor="pageSelector" className="mr-2">
+                                Select Page:
+                            </label>
+                            <select
+                                id="pageSelector"
+                                value={currentPage}
+                                onChange={handlePageChange}
+                            >
+                                <option value={1}>Page 1</option>
+                                <option value={2}>Page 2</option>
+                                <option value={3}>Page 3</option>
+                                <option value={4}>Page 4</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                {currentPage === 1 && (
+                    <EditFormPage1
+                        questions={questions}
+                        question={question}
+                        handleInputChange={handleInputChange}
+                        handleDropdownChange={handleDropdownChange}
+                        addDropdownChoice={addDropdownChoice}
+                        removeDropdownChoice={removeDropdownChoice}
+                        addQuestion={addQuestion}
+                        editQuestion={editQuestion}
+                        deleteQuestion={deleteQuestion}
+                        error={error}
+                    />
+                )}
+                {currentPage === 2 && (
+                    <EditFormPage2
+                        questions={questions}
+                        question={question}
+                        handleInputChange={handleInputChange}
+                        handleDropdownChange={handleDropdownChange}
+                        addDropdownChoice={addDropdownChoice}
+                        removeDropdownChoice={removeDropdownChoice}
+                        addQuestion={addQuestion}
+                        editQuestion={editQuestion}
+                        deleteQuestion={deleteQuestion}
+                        error={error}
+                    />
+                )}
+                {currentPage === 3 && (
+                    <EditFormPage3
+                        questions={questions}
+                        question={question}
+                        handleInputChange={handleInputChange}
+                        handleDropdownChange={handleDropdownChange}
+                        addDropdownChoice={addDropdownChoice}
+                        removeDropdownChoice={removeDropdownChoice}
+                        addQuestion={addQuestion}
+                        editQuestion={editQuestion}
+                        deleteQuestion={deleteQuestion}
+                        error={error}
+                    />
+                )}
+                {currentPage === 4 && (
+                    <EditFormPage4
+                        questions={questions}
+                        question={question}
+                        handleInputChange={handleInputChange}
+                        handleDropdownChange={handleDropdownChange}
+                        addDropdownChoice={addDropdownChoice}
+                        removeDropdownChoice={removeDropdownChoice}
+                        addQuestion={addQuestion}
+                        editQuestion={editQuestion}
+                        deleteQuestion={deleteQuestion}
+                        error={error}
+                    />
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default EditManageQuestions;
